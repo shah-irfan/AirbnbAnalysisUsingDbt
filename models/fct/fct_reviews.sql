41ef2bee4b944a0d07d22a626b9a95c5bf79ff0d
Shahul Irfan <shahulirfan4u@gmail.com>
2024-11-07 12:19:46 +0530

    Initial commit

---
+++ models/fct/fct_reviews.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+    materialized = 'incremental',
+    on_schema_change='fail'
+    )
+}}
+WITH src_reviews AS (
+    SELECT * FROM {{ ref('src_reviews')}}
+)
+SELECT
+    *
+FROM
+    src_reviews
+WHERE
+    review_text is NOT NULL
+{% if is_incremental() %}
+    AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
